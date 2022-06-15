a39e6b406d1a6dc675dad174b3fcce2fb454f960
Javier Ortiz <javier.ortiz13@upr.edu>
2022-06-15 13:09:09 -0700

    Added a doc directory just so I could save my presentation script

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, make_response
 import sys
 import csv
 from flask_cors import CORS
+import traceback
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +10,10 @@ CORS(app)
 @app.route('/',methods = ['POST', 'GET'] )
 def index():
     if request.method == 'GET':
-        company_id = request.args.get('company-id')
+        company_name = request.args.get('company-id')
+        company_name = ''.join(company_name.split()).lower()
         selected = request.args.get('selected_cypher')
-        if company_id:
+        if company_name:
             investors_file = "investors.csv"
             peers_file = "peers.csv"
             industry_file = "company_industry.csv"
@@ -19,6 +21,10 @@ def index():
             try:
                 with open(investors_file) as invest:
                     with open(peers_file) as peer:
+                        reader = csv.reader(peer)
+                        next(reader, None) # skip headers
+                        name_id_map = dict((''.join(rows[1].split()).lower(), rows[0]) for rows in reader)
+                        peer.seek(0) # back to top of file
                         with open(industry_file) as industries:
                             reader = csv.reader(industries)
                             next(reader, None) # skip headers
@@ -27,9 +33,11 @@ def index():
                                 reader = csv.reader(scores)
                                 next(reader, None) # skip headers
                                 score_map = dict((rows[0],rows[1]) for rows in reader)
+                                company_id = name_id_map[company_name]
                                 return render_template("index.html", investors=invest, peers=peer, company_id=company_id, selected=selected, industries=industry_map, score=score_map)
-            except:
-                return render_template("index.html", company_id=company_id)
+            except Exception as e:
+                traceback.print_exc()
+                return render_template("index.html")
         else:
             return render_template("index.html")
     return render_template("index.html")
